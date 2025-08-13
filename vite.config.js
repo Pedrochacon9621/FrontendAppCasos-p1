bb0a4b3df8e89b1a17064257d44c68c0886446b1
Pedro Chacón <pedrochacon9621@gmail.com>
2025-08-13 18:25:40 +0200

    devolviendo el base1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // Esto hace falta para render hosting
+  base: '/', // Esto hace falta para render hosting
   plugins: [react()],
 })
